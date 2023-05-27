016a92cefb7e937dab203d0c261af34b67429991
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-05-26 23:20:13 -0500

    added comments and print statements

--- javaATS/src/javagreeting/HelloWorld.java
+++ javaATS/src/javagreeting/HelloWorld.java
@@ -1,10 +1,11 @@
 package javagreeting;
-
+/*HelloWorld.java - Printing statements and trying comments*/
 public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Here's Java!");
-		
+		System.out.println("Remember: type syso and hit ctrl + space to automatically type System.Out.println()");
+		System.out.println("Always be efficient!");
 		
 	}
 
