12c1ec15d8771cf4abba1ec83c30f8ab05997bac
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-05-30 14:36:22 -0500

    added exercise to check prime numbers

---
+++ javaATS/src/introduccion/IsPrime.java
@@ -0,0 +1,29 @@
+package introduccion;
+// Checks which numbers within a range are prime numbers
+public class IsPrime {
+	public static void main(String[] argv){
+		
+		for (int i = 1001; i < 1050; i++) {
+			
+			//Iterate through each value to check if its prime
+			boolean itsPrime = true;
+			for (int j = 2; j <= i/2; j++) {
+				
+				//if i can be divided by 1, then it is not a prime number
+				
+				if (i % j == 0) {
+					itsPrime = false;
+					break;
+				}
+			}
+			
+			//Now we print the prime numbers
+			if (itsPrime) {
+				System.out.println(i);
+					}
+			
+			
+			
+		}
+	}
+}
